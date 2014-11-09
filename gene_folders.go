2d60fe1a963d510b1845136244141eaf7d490bcd
Cihangir SAVAS <cihangir@koding.com>
2014-11-10 00:30:42 +0200

    Gene: added folder structure generator

---
+++ gene_folders.go
@@ -0,0 +1,46 @@
+package gene
+
+import (
+	"os"
+	"path"
+)
+
+var folderStucture = []string{
+	"gene",
+	"gene/modules",
+	"gene/modules/",
+	"gene/models",
+}
+
+func EnsureFolders(root string, folderStucture []string) error {
+	for _, folder := range folderStucture {
+		path := path.Join(root, folder)
+		exists, err := exists(path)
+		if err != nil {
+			return err
+		}
+
+		if exists {
+			continue
+		}
+
+		if err := os.Mkdir(path, os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// check if folder or file exists
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
