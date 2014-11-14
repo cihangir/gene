611ccc13508f866a6e45fe5837fe21c4a197dc3f
Cihangir SAVAS <cihangir@koding.com>
2014-11-14 23:47:55 +0200

    Gene: added custom handlers

---
+++ apiiniter.go
@@ -0,0 +1,13 @@
+package gene
+
+import "github.com/rcrowley/go-tigertonic"
+
+// Initer is for initializing the api endpoints for a module
+type Initer interface {
+	Iinit(mux *tigertonic.TrieServeMux) *tigertonic.TrieServeMux
+}
+
+// Customizer is for adding custom handlers to the module
+type Customizer interface {
+	Customize(mux *tigertonic.TrieServeMux) *tigertonic.TrieServeMux
+}
