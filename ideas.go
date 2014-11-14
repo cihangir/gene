3ef8a7ee554737fcc34d928df5ace19eda13b7ee
Cihangir SAVAS <cihangir@koding.com>
2014-11-14 23:48:03 +0200

    Ideas: added some ideas

---
+++ ideas.go
@@ -0,0 +1,10 @@
+package gene
+
+// Doc generation
+// http://beego.me/docs/advantage/docs.md
+// https://github.com/yvasiyarov/swagger
+
+// Code generation
+// http://clipperhouse.github.io/gen/#List
+
+// Configuration change management
