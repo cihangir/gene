35d693a709febbaf6f023305afba03e16b310b1f
Cihangir SAVAS <cihangir@koding.com>
2014-11-15 13:35:15 +0200

    Handlers: create a base handler

---
+++ handler/handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/rcrowley/go-tigertonic"
+)
+
+var Cors = tigertonic.NewCORSBuilder().AddAllowedOrigins("*")
+
+type Router struct {
+	Name    string
+	Handler interface{}
+}
+
+func Wrapper(r Router) http.Handler {
+
+	var httpHandler http.Handler
+
+	httpHandler = tigertonic.Marshaled(r.Handler)
+
+	// todo set registery here for timers
+	httpHandler = tigertonic.Timed(httpHandler, r.Name, nil)
+
+	// create the final handler
+	// TODO ~ make those configurable
+	return Cors.Build(httpHandler)
+}
