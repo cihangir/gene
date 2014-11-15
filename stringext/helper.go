7a1e7fb78f75e0f7a950ca4aa8fd20a6b9d05cf6
Cihangir SAVAS <cihangir@koding.com>
2014-11-15 13:34:10 +0200

    Stringext: create extension functions to strings package

---
+++ stringext/helper.go
@@ -0,0 +1,87 @@
+package stringext
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+var (
+	camelcase = regexp.MustCompile(`(?m)[-.$/:_{}\s]`)
+	acronyms  = regexp.MustCompile(`(Url|Http|Id|Io|Uuid|Api|Uri|Ssl|Cname|Oauth|Otp)$`)
+)
+
+func ToLowerFirst(ident string) string {
+	r, n := utf8.DecodeRuneInString(ident)
+	return string(unicode.ToLower(r)) + ident[n:]
+}
+
+func ToUpperFirst(ident string) string {
+	r, n := utf8.DecodeRuneInString(ident)
+	return string(unicode.ToUpper(r)) + ident[n:]
+}
+
+func JSONTag(n string, required bool) string {
+	tags := []string{ToLowerFirst(n)}
+	if !required {
+		tags = append(tags, "omitempty")
+	}
+
+	return fmt.Sprintf("`json:\"%s\"`", strings.Join(tags, ","))
+}
+
+func AsComment(c string) string {
+	var buf bytes.Buffer
+	const maxLen = 70
+	removeNewlines := func(s string) string {
+		return strings.Replace(s, "\n", "\n// ", -1)
+	}
+	for len(c) > 0 {
+		line := c
+		if len(line) < maxLen {
+			fmt.Fprintf(&buf, "// %s\n", removeNewlines(line))
+			break
+		}
+		line = line[:maxLen]
+		si := strings.LastIndex(line, " ")
+		if si != -1 {
+			line = line[:si]
+		}
+		fmt.Fprintf(&buf, "// %s\n", removeNewlines(line))
+		c = c[len(line):]
+		if si != -1 {
+			c = c[1:]
+		}
+	}
+
+	return buf.String()
+}
+
+func Contains(n string, r []string) bool {
+	for _, r := range r {
+		if r == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Depunct(ident string, initialCap bool) string {
+	matches := camelcase.Split(ident, -1)
+	for i, m := range matches {
+		if initialCap || i > 0 {
+			m = ToUpperFirst(m)
+		}
+		matches[i] = acronyms.ReplaceAllStringFunc(m, func(c string) string {
+			if len(c) > 4 {
+				return strings.ToUpper(c[:2]) + c[2:]
+			}
+			return strings.ToUpper(c)
+		})
+	}
+	return strings.Join(matches, "")
+}
---
+++ stringext/helpers_test.go
@@ -0,0 +1,38 @@
+package stringext
+
+import "testing"
+
+var initialCapTests = []struct {
+	Ident     string
+	Depuncted string
+}{
+	{
+		Ident:     "provider_id",
+		Depuncted: "ProviderID",
+	},
+	{
+		Ident:     "app-identity",
+		Depuncted: "AppIdentity",
+	},
+	{
+		Ident:     "uuid",
+		Depuncted: "UUID",
+	},
+	{
+		Ident:     "oauth-client",
+		Depuncted: "OAuthClient",
+	},
+	{
+		Ident:     "Dyno all",
+		Depuncted: "DynoAll",
+	},
+}
+
+func TestInitialCap(t *testing.T) {
+	for i, ict := range initialCapTests {
+		depuncted := Depunct(ict.Ident, true)
+		if depuncted != ict.Depuncted {
+			t.Errorf("%d: wants %v, got %v", i, ict.Depuncted, depuncted)
+		}
+	}
+}
