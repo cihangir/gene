5e11d02c5f7bbabe7b45b09fc8804c35eab0e935
Cihangir SAVAS <cihangir@koding.com>
2014-11-15 13:32:59 +0200

    Folders: create a new generator for models

---
+++ generators/models/models_test.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"encoding/json"
+
+	"testing"
+
+	"bitbucket.org/cihangirsavas/gene/generators/folders"
+	"bitbucket.org/cihangirsavas/gene/schema"
+	"bitbucket.org/cihangirsavas/gene/testdata"
+	"bitbucket.org/cihangirsavas/gene/writers"
+)
+
+func TestGenerateModel(t *testing.T) {
+	var s schema.Schema
+	if err := json.Unmarshal([]byte(testdata.JSON1), &s); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	model, err := s.GenerateModel()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	folders.EnsureFolders("/tmp/", folders.FolderStucture)
+	fileName := "./gene/models/" + s.Title + ".go"
+
+	err = writers.WriteFormattedFile(fileName, model)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+}
