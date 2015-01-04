b8cfede21bf0ac39b367eec169a85c758fc3824c
Cihangir SAVAS <cihangir@koding.com>
2015-01-04 00:49:04 -0800

    Generators/Errors: generate errors with the definition section

--- generators/errors/template.go
+++ generators/errors/template.go
@@ -3,8 +3,10 @@ package errors
 var ErrorsTemplate = `
 package errs
 var (
-{{range $key, $value := .Properties}}
-    Err{{$key}}NotSet = errors.New("{{$key}} not set")
+{{range $defKey, $def := .Definitions}}
+    {{range $key, $value := $def.Properties}}
+        Err{{$def.Title}}{{$value.Title}}NotSet = errors.New("{{$def.Title}} {{$value.Title}} not set")
+    {{end}}
 {{end}}
 )
 `
