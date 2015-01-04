00511fe99a8a8a37f87a7dd73652174b9e0b5464
Cihangir SAVAS <cihangir@koding.com>
2015-01-04 01:13:32 -0800

    Generator/Errors: update error template

--- generators/errors/template.go
+++ generators/errors/template.go
@@ -5,7 +5,7 @@ package errs
 var (
 {{range $defKey, $def := .Definitions}}
     {{range $key, $value := $def.Properties}}
-        Err{{$def.Title}}{{$value.Title}}NotSet = errors.New("{{$def.Title}} {{$value.Title}} not set")
+        Err{{$def.Title}}{{$key}}NotSet = errors.New("{{$def.Title}}.{{$key}} not set")
     {{end}}
 {{end}}
 )
