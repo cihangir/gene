8be4d4350d31228838e5479a7bd7ed5dce7686d5
Cihangir SAVAS <cihangir@koding.com>
2015-01-11 01:55:25 -0800

    Stringext: massive test operatoins

---
+++ stringext/stringext.go
@@ -0,0 +1,160 @@
+package stringext
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+var (
+	specialCases = regexp.MustCompile(`(?m)[-.$/:_{}\s]`)
+	acronyms     = regexp.MustCompile(`(Url|Http|Id|Io|Uuid|Api|Uri|Ssl|Cname|Oauth|Otp)$`)
+	acronymsi    = regexp.MustCompile(`(URL|HTTP|ID|IO|UUID|API|URI|SSL|CNAME|OAUTH|OTP)`)
+)
+
+// ToLowerFirst lowers the first character of any given unicode char
+func ToLowerFirst(ident string) string {
+	r, n := utf8.DecodeRuneInString(ident)
+	return string(unicode.ToLower(r)) + ident[n:]
+}
+
+// ToUpperFirst converts the first character of any given unicode char to
+// uppercase
+func ToUpperFirst(ident string) string {
+	r, n := utf8.DecodeRuneInString(ident)
+	return string(unicode.ToUpper(r)) + ident[n:]
+}
+
+// Pointerize returns the first character of a given string as lowercased, this
+// method is intened to use as a function receiver generator
+func Pointerize(ident string) string {
+	r, _ := utf8.DecodeRuneInString(ident)
+	return string(unicode.ToLower(r))
+}
+
+// JSONTag generates json tag for given string, it is using the javascript
+// concepts
+//
+// eg: ID ->
+// 	becomes "id" if it is at the beginning
+//  	or
+//  becomes "Id" if it is in the middle of the string
+func JSONTag(n string, required bool) string {
+	modified := ToLowerFirst(
+		Normalize(
+			DepunctWithInitialLower(n),
+		),
+	)
+
+	tags := []string{modified}
+	if !required {
+		tags = append(tags, "omitempty")
+	}
+
+	return fmt.Sprintf("`json:\"%s\"`", strings.Join(tags, ","))
+}
+
+// Normalize removes non a-z characters and uppercases the following character,
+// all characters followed by it will be lowercased if the word is one the
+// acronymsi
+func Normalize(s string) string {
+	return acronymsi.ReplaceAllStringFunc(
+		DepunctWithInitialLower(s), func(c string) string {
+			return ToUpperFirst(strings.ToLower(c))
+		})
+}
+
+// ToFieldName handles field names, if the given string is one of the
+// `acronymsi` it is lowercasing it
+//
+// given "URL" as parameter converted to "url"
+// given "ProfileURL" as parameter converted to "profile_url"
+// given "Profile" as parameter converted to "profile"
+// given "ProfileName" as parameter converted to "profile_name"
+//
+func ToFieldName(u string) string {
+	buf := bytes.NewBufferString("")
+	for i, v := range Normalize(u) {
+		if i > 0 && v >= 'A' && v <= 'Z' {
+			buf.WriteRune('_')
+		}
+		buf.WriteRune(v)
+	}
+
+	return strings.ToLower(buf.String())
+}
+
+// DepunctWithInitialUpper does special operations to the given string, while
+// operating uppercases the special words
+func DepunctWithInitialUpper(ident string) string {
+	return Depunct(ident, true)
+}
+
+// DepunctWithInitialUpper does special operations to the given string, while
+// operating lowercases the special words
+func DepunctWithInitialLower(ident string) string {
+	return Depunct(ident, false)
+}
+
+// Depunct splits the given string with special chars and operates on them one
+// by one
+func Depunct(ident string, initialCap bool) string {
+	matches := specialCases.Split(ident, -1)
+	for i, m := range matches {
+		if initialCap || i > 0 {
+			m = ToUpperFirst(m)
+		}
+		matches[i] = acronyms.ReplaceAllStringFunc(m, func(c string) string {
+			if len(c) > 4 {
+				return strings.ToUpper(c[:2]) + c[2:]
+			}
+			return strings.ToUpper(c)
+		})
+	}
+	return strings.Join(matches, "")
+}
+
+// Equal check if given two strings are same, used in templates
+func Equal(a, b string) bool {
+	return a == b
+}
+
+func AsComment(c string) string {
+	var buf bytes.Buffer
+	const maxLen = 70
+	removeNewlines := func(s string) string {
+		return strings.Replace(s, "\n", "\n// ", -1)
+	}
+	for len(c) > 0 {
+		line := c
+		if len(line) < maxLen {
+			fmt.Fprintf(&buf, "// %s\n", removeNewlines(line))
+			break
+		}
+		line = line[:maxLen]
+		si := strings.LastIndex(line, " ")
+		if si != -1 {
+			line = line[:si]
+		}
+		fmt.Fprintf(&buf, "// %s\n", removeNewlines(line))
+		c = c[len(line):]
+		if si != -1 {
+			c = c[1:]
+		}
+	}
+
+	return buf.String()
+}
+
+func Contains(n string, r []string) bool {
+	for _, r := range r {
+		if r == n {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ stringext/stringext_test.go
@@ -0,0 +1,166 @@
+package stringext
+
+import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+var testData = []struct {
+	Value                   string
+	ToLowerFirst            string
+	ToUpperFirst            string
+	Pointerize              string
+	JSONTag                 string
+	JSONTagRequired         string
+	Normalize               string
+	ToFieldName             string
+	DepunctWithInitialUpper string
+	DepunctWithInitialLower string
+}{
+	{
+		Value:                   "name",
+		ToLowerFirst:            "name",
+		ToUpperFirst:            "Name",
+		Pointerize:              "n",
+		JSONTag:                 "`json:\"name,omitempty\"`",
+		JSONTagRequired:         "`json:\"name\"`",
+		Normalize:               "name",
+		ToFieldName:             "name",
+		DepunctWithInitialUpper: "Name",
+		DepunctWithInitialLower: "name",
+	},
+	{
+		Value:                   "provider_id",
+		ToLowerFirst:            "provider_id",
+		ToUpperFirst:            "Provider_id",
+		Pointerize:              "p",
+		JSONTag:                 "`json:\"providerId,omitempty\"`",
+		JSONTagRequired:         "`json:\"providerId\"`",
+		Normalize:               "providerId",
+		ToFieldName:             "provider_id",
+		DepunctWithInitialUpper: "ProviderID",
+		DepunctWithInitialLower: "providerID",
+	},
+	{
+		Value:                   "app-identity",
+		ToLowerFirst:            "app-identity",
+		ToUpperFirst:            "App-identity",
+		Pointerize:              "a",
+		JSONTag:                 "`json:\"appIdentity,omitempty\"`",
+		JSONTagRequired:         "`json:\"appIdentity\"`",
+		Normalize:               "appIdentity",
+		ToFieldName:             "app_identity",
+		DepunctWithInitialUpper: "AppIdentity",
+		DepunctWithInitialLower: "appIdentity",
+	},
+	{
+		Value:                   "uuid",
+		ToLowerFirst:            "uuid",
+		ToUpperFirst:            "Uuid",
+		Pointerize:              "u",
+		JSONTag:                 "`json:\"uuid,omitempty\"`",
+		JSONTagRequired:         "`json:\"uuid\"`",
+		Normalize:               "uuid",
+		ToFieldName:             "uuid",
+		DepunctWithInitialUpper: "UUID",
+		DepunctWithInitialLower: "uuid",
+	},
+	{
+		Value:                   "oauth-client",
+		ToLowerFirst:            "oauth-client",
+		ToUpperFirst:            "Oauth-client",
+		Pointerize:              "o",
+		JSONTag:                 "`json:\"oauthClient,omitempty\"`",
+		JSONTagRequired:         "`json:\"oauthClient\"`",
+		Normalize:               "oauthClient",
+		ToFieldName:             "oauth_client",
+		DepunctWithInitialUpper: "OAuthClient",
+		DepunctWithInitialLower: "oauthClient",
+	},
+	{
+		Value:                   "Dyno all",
+		ToLowerFirst:            "dyno all",
+		ToUpperFirst:            "Dyno all",
+		Pointerize:              "d",
+		JSONTag:                 "`json:\"dynoAll,omitempty\"`",
+		JSONTagRequired:         "`json:\"dynoAll\"`",
+		Normalize:               "DynoAll",
+		ToFieldName:             "dyno_all",
+		DepunctWithInitialUpper: "DynoAll",
+		DepunctWithInitialLower: "DynoAll",
+	},
+}
+
+func TestToLowerFirst(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.ToLowerFirst, ToLowerFirst(ict.Value))
+	}
+}
+
+func TestUpperFirst(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.ToUpperFirst, ToUpperFirst(ict.Value))
+	}
+}
+
+func TestPointerize(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.Pointerize, Pointerize(ict.Value))
+	}
+}
+
+func TestJSONTag(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.JSONTag, JSONTag(ict.Value, false))
+	}
+}
+
+func TestJSONTagRequired(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.JSONTagRequired, JSONTag(ict.Value, true))
+	}
+}
+
+func TestNormalize(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.Normalize, Normalize(ict.Value))
+	}
+}
+
+func TestToFieldName(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.ToFieldName, ToFieldName(ict.Value))
+	}
+}
+
+func TestDepunctWithInitialUpper(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.DepunctWithInitialUpper, DepunctWithInitialUpper(ict.Value))
+	}
+}
+
+func TestDepunctWithInitialLower(t *testing.T) {
+	for _, ict := range testData {
+		equals(t, ict.DepunctWithInitialLower, DepunctWithInitialLower(ict.Value))
+	}
+}
+
+// func TestInitialCap(t *testing.T) {
+// 	for i, ict := range initialCapTests {
+// 		depuncted := Depunct(ict.Ident, true)
+// 		if depuncted != ict.Depuncted {
+// 			t.Errorf("%d: wants %v, got %v", i, ict.Depuncted, depuncted)
+// 		}
+// 	}
+// }
+
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.Fail()
+	}
+}
