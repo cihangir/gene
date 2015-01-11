d7caa039090af13fe1939a1167cc41734af7d711
Cihangir SAVAS <cihangir@koding.com>
2015-01-11 13:37:06 -0800

    Constants: create tests for constants

---
+++ generators/constants/constants_test.go
@@ -0,0 +1,43 @@
+package constants
+
+import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"runtime"
+
+	"testing"
+
+	"github.com/cihangir/gene/schema"
+	"github.com/cihangir/gene/testdata"
+)
+
+const expected = `
+	// StatusConstant holds the predefined enums
+	const StatusConstant = struct {
+		Active  string
+		Deleted string
+	}{
+		Active:  "active",
+		Deleted: "deleted",
+	}`
+
+func TestConstants(t *testing.T) {
+	var s schema.Schema
+	if err := json.Unmarshal([]byte(testdata.JSON1), &s); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	a, err := Generate(&s)
+	equals(t, nil, err)
+	equals(t, expected, string(a))
+}
+
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.Fail()
+	}
+}
