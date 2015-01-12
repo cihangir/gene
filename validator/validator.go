eafe36bd05d177f5cc8b8889a2212701ec6651df
Cihangir SAVAS <cihangir@koding.com>
2015-01-11 20:35:14 -0800

    Validator: they should satisfy Validate interface

--- validator/validator.go
+++ validator/validator.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"regexp"
 	"strings"
+	"time"
 	"unicode/utf8"
 )
 
@@ -23,6 +24,8 @@ var (
 	ERR_MSG_NUMBER_MUST_BE_GREATER = `must be greater than %f`
 	ERR_MSG_NUMBER_MUST_BE_LOWER   = `must be lower than %f`
 	ERR_MSG_MULTIPLE_OF            = `must be a multiple of %f`
+
+	ERR_MSG_DATE = `date should not be zero`
 
 	// ERR_MSG_NUMBER_MUST_BE_LOWER_OR_EQUAL   = `must be lower than or equal to %s`
 	// ERR_MSG_NUMBER_MUST_BE_GREATER_OR_EQUAL = `must be greater than or equal to %f`
@@ -46,66 +49,130 @@ var (
 	// ERR_MSG_INVALID_PATTERN_PROPERTY        = `property "%s" does not match pattern %s`
 )
 
-func MinLength(data string, length int) error {
-	if utf8.RuneCount([]byte(data)) < length {
-		return fmt.Errorf(ERR_MSG_STRING_LENGTH_MUST_BE_GREATER_OR_EQUAL, length)
-	}
-
-	return nil
+type Validator interface {
+	Validate() error
 }
 
-func MaxLength(data string, length int) error {
-	if utf8.RuneCount([]byte(data)) > length {
-		return fmt.Errorf(ERR_MSG_STRING_LENGTH_MUST_BE_LOWER_OR_EQUAL, length)
-	}
-
-	return nil
+type valide struct {
+	f func() error
 }
 
-func Pattern(data string, pattern string) error {
-	// TODO add caching for compile?
-	regex, err := regexp.Compile(pattern)
-	if err != nil {
-		return err
-	}
-
-	if !regex.MatchString(data) {
-		return fmt.Errorf(ERR_MSG_DOES_NOT_MATCH_PATTERN, pattern)
-	}
-
-	return nil
+func (v *valide) Validate() error {
+	return v.f()
 }
 
-func OneOf(data string, enums []string) error {
-	for _, val := range enums {
-		if val == data {
+func MinLength(data string, length int) Validator {
+	return &valide{
+		f: func() error {
+			if utf8.RuneCount([]byte(data)) < length {
+				return fmt.Errorf(ERR_MSG_STRING_LENGTH_MUST_BE_GREATER_OR_EQUAL, length)
+			}
+
 			return nil
-		}
+		},
 	}
-
-	return fmt.Errorf(ERR_MSG_MUST_MATCH_ONE_ENUM_VALUES, strings.Join(enums, ","))
 }
 
-func Min(data float64, min float64) error {
-	if data < min {
-		return fmt.Errorf(ERR_MSG_NUMBER_MUST_BE_GREATER, min)
-	}
+func MaxLength(data string, length int) Validator {
+	return &valide{
+		f: func() error {
+			if utf8.RuneCount([]byte(data)) > length {
+				return fmt.Errorf(ERR_MSG_STRING_LENGTH_MUST_BE_LOWER_OR_EQUAL, length)
+			}
 
-	return nil
+			return nil
+		},
+	}
 }
 
-func Max(data float64, max float64) error {
-	if data > max {
-		return fmt.Errorf(ERR_MSG_NUMBER_MUST_BE_LOWER, max)
-	}
+func Pattern(data string, pattern string) Validator {
+	return &valide{
+		f: func() error {
+			// TODO add caching for compile?
+			regex, err := regexp.Compile(pattern)
+			if err != nil {
+				return err
+			}
 
-	return nil
+			if !regex.MatchString(data) {
+				return fmt.Errorf(ERR_MSG_DOES_NOT_MATCH_PATTERN, pattern)
+			}
+
+			return nil
+		},
+	}
+}
+func OneOf(data string, enums []string) Validator {
+	return &valide{
+		f: func() error {
+			for _, val := range enums {
+				if val == data {
+					return nil
+				}
+			}
+
+			return fmt.Errorf(ERR_MSG_MUST_MATCH_ONE_ENUM_VALUES, strings.Join(enums, ","))
+		},
+	}
 }
 
-func MultipleOf(data float64, multipleOf float64) error {
-	if math.Mod(data, multipleOf) == 0 {
-		return fmt.Errorf(ERR_MSG_MULTIPLE_OF, multipleOf)
-	}
+func Min(data float64, min float64) Validator {
+	return &valide{
+		f: func() error {
+			if data < min {
+				return fmt.Errorf(ERR_MSG_NUMBER_MUST_BE_GREATER, min)
+			}
 
-	return nil
+			return nil
+		},
+	}
+}
+func Max(data float64, max float64) Validator {
+	return &valide{
+		f: func() error {
+			if data > max {
+				return fmt.Errorf(ERR_MSG_NUMBER_MUST_BE_LOWER, max)
+			}
+
+			return nil
+		},
+	}
+}
+
+func MultipleOf(data float64, multipleOf float64) Validator {
+	return &valide{
+		f: func() error {
+			if math.Mod(data, multipleOf) == 0 {
+				return fmt.Errorf(ERR_MSG_MULTIPLE_OF, multipleOf)
+			}
+
+			return nil
+		},
+	}
+}
+
+func Date(data time.Time) Validator {
+	return &valide{
+		f: func() error {
+			if data.IsZero() {
+				return fmt.Errorf(ERR_MSG_DATE)
+			}
+
+			return nil
+		},
+	}
+}
+
+func NewMulti(v ...Validator) Validator {
+	return &valide{
+		f: func() error {
+			for _, vv := range v {
+				if err := vv.Validate(); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		},
+	}
 }
