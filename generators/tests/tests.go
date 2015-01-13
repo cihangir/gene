8577df4391dc333809b94a8544a7e5eb65772b4d
Cihangir SAVAS <cihangir@koding.com>
2015-01-13 01:56:32 -0800

    Generator: write tests for tests

--- generators/tests/tests.go
+++ generators/tests/tests.go
@@ -6,95 +6,105 @@ import (
 	"strings"
 	"text/template"
 
+	"go/format"
+
+	"github.com/cihangir/gene/generators/common"
 	"github.com/cihangir/gene/schema"
 	"github.com/cihangir/gene/stringext"
 	"github.com/cihangir/gene/writers"
 )
 
 func Generate(rootPath string, s *schema.Schema) error {
-	if err := GenerateTestFuncs(rootPath); err != nil {
-		return err
-	}
-
-	if err := GenerateMainTestFileForModule(rootPath, s); err != nil {
-		return err
-	}
-
-	for _, def := range s.Definitions {
-		err := GenerateTests(rootPath, s.Title, def.Title)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func GenerateMainTestFileForModule(
-	rootPath string,
-	s *schema.Schema,
-) error {
-	// TODO check if file is there, no need to continue
-
-	temp := template.New("mainTestFile.tmpl")
-	temp.Funcs(template.FuncMap{
-		"ToLowerFirst": stringext.ToLowerFirst,
-		"ToLower":      strings.ToLower,
-		"ToUpperFirst": stringext.ToUpperFirst,
-	})
-
-	_, err := temp.Parse(MainTestsTemplate)
-	if err != nil {
-		return err
-	}
-
-	var buf bytes.Buffer
-
-	err = temp.ExecuteTemplate(&buf, "mainTestFile.tmpl", s)
-	if err != nil {
-		return err
-	}
-
-	path := fmt.Sprintf(
-		"%sworkers/%s/tests/common_test.go",
-		rootPath,
-		strings.ToLower(s.Title),
-	)
-
-	return writers.WriteFormattedFile(path, buf.Bytes())
-}
-
-func GenerateTestFuncs(rootPath string) error {
-	// TODO check if file is there, no need to continue
-	temp := template.New("testFuncs.tmpl")
-	_, err := temp.Parse(TestFuncs)
-	if err != nil {
-		return err
-	}
-
-	var buf bytes.Buffer
-
-	err = temp.ExecuteTemplate(&buf, "testFuncs.tmpl", nil)
+	// Generate test functions
+	testFuncs, err := GenerateTestFuncs(s)
 	if err != nil {
 		return err
 	}
 
 	path := fmt.Sprintf("%stests/testfuncs.go", rootPath)
 
-	return writers.WriteFormattedFile(path, buf.Bytes())
-}
+	if err := writers.WriteFormattedFile(path, testFuncs); err != nil {
+		return err
+	}
 
-func GenerateTests(rootPath string, moduleName string, name string) error {
-	temp := template.New("tests.tmpl")
-	temp.Funcs(template.FuncMap{
-		"ToLowerFirst": stringext.ToLowerFirst,
-		"ToLower":      strings.ToLower,
-		"ToUpperFirst": stringext.ToUpperFirst,
-	})
-
-	_, err := temp.Parse(TestsTemplate)
+	// generate module test file
+	mainTest, err := GenerateMainTestFileForModule(s)
 	if err != nil {
 		return err
+	}
+
+	path = fmt.Sprintf(
+		"%sworkers/%s/tests/common_test.go",
+		rootPath,
+		strings.ToLower(s.Title),
+	)
+
+	if err := writers.WriteFormattedFile(path, mainTest); err != nil {
+		return err
+	}
+
+	// generate tests for the schema
+	for _, def := range s.Definitions {
+		testFile, err := GenerateTests(s.Title, def.Title)
+		if err != nil {
+			return err
+		}
+		path = fmt.Sprintf(
+			"%sworkers/%s/tests/%s_test.go",
+			rootPath,
+			s.Title,
+			stringext.ToLowerFirst(def.Title),
+		)
+
+		return writers.WriteFormattedFile(path, testFile)
+	}
+
+	return nil
+}
+
+func GenerateMainTestFileForModule(s *schema.Schema) ([]byte, error) {
+	// TODO check if file is there, no need to continue
+
+	temp := template.New("mainTestFile.tmpl").Funcs(common.TemplateFuncs)
+
+	_, err := temp.Parse(MainTestsTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+
+	err = temp.ExecuteTemplate(&buf, "mainTestFile.tmpl", s)
+	if err != nil {
+		return nil, err
+	}
+
+	return format.Source(buf.Bytes())
+}
+
+func GenerateTestFuncs(s *schema.Schema) ([]byte, error) {
+	// TODO check if file is there, no need to continue
+	temp := template.New("testFuncs.tmpl")
+	_, err := temp.Parse(TestFuncs)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+
+	err = temp.ExecuteTemplate(&buf, "testFuncs.tmpl", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return format.Source(buf.Bytes())
+}
+
+func GenerateTests(moduleName string, name string) ([]byte, error) {
+	temp := template.New("tests.tmpl").Funcs(common.TemplateFuncs)
+	_, err := temp.Parse(TestsTemplate)
+	if err != nil {
+		return nil, err
 	}
 
 	var buf bytes.Buffer
@@ -109,15 +119,8 @@ func GenerateTests(rootPath string, moduleName string, name string) error {
 
 	err = temp.ExecuteTemplate(&buf, "tests.tmpl", data)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	path := fmt.Sprintf(
-		"%sworkers/%s/tests/%s_test.go",
-		rootPath,
-		moduleName,
-		stringext.ToLowerFirst(name),
-	)
-
-	return writers.WriteFormattedFile(path, buf.Bytes())
+	return format.Source(buf.Bytes())
 }
