6620d806a080bed6dcb6e13ea8b15441e40fca0e
Cihangir SAVAS <cihangir@koding.com>
2015-05-24 14:16:07 -0700

    Gene: create separate test cases for sql generators

---
+++ generators/sql/definitions/sequence_test.go
@@ -0,0 +1,52 @@
+package definitions
+
+import (
+	"encoding/json"
+
+	"testing"
+
+	"github.com/cihangir/gene/config"
+	"github.com/cihangir/gene/testdata"
+	"github.com/cihangir/schema"
+	"github.com/cihangir/stringext"
+)
+
+func TestSequence(t *testing.T) {
+	s := &schema.Schema{}
+	if err := json.Unmarshal([]byte(testdata.TestDataFull), s); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	s = s.Resolve(s)
+
+	context := config.NewContext()
+	moduleName := context.ModuleNameFunc(s.Title)
+
+	index := 0
+	for _, def := range s.Definitions {
+
+		// schema should have our generator
+		if !def.Generators.Has(generatorName) {
+			continue
+		}
+
+		settings, _ := def.Generators.Get(generatorName)
+		settings.SetNX("schemaName", stringext.ToFieldName(moduleName))
+		settings.SetNX("tableName", stringext.ToFieldName(def.Title))
+		settings.SetNX("roleName", stringext.ToFieldName(moduleName))
+
+		sts := DefineSequence(settings, def)
+		equals(t, expectedSequences[index], sts)
+		index++
+	}
+}
+
+var expectedSequences = []string{
+	`
+-- ----------------------------
+--  Sequence structure for account.profile_id
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "account"."profile_id_seq" CASCADE;
+CREATE SEQUENCE "account"."profile_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
+GRANT USAGE ON SEQUENCE "account"."profile_id_seq" TO "social";`,
+}
