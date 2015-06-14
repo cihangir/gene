9896c2105774f42104bd9e83748479693207893c
Cihangir SAVAS <cihangir@koding.com>
2015-06-14 01:01:08 -0700

    Gene: added primary key to profile

---
+++ example/twitter/db/account/008-profile-constraints.sql
@@ -0,0 +1,4 @@
+-------------------------------
+--  Primary key structure for table profile
+-- ----------------------------
+ALTER TABLE "account"."profile" ADD PRIMARY KEY ("id") NOT DEFERRABLE INITIALLY IMMEDIATE;
