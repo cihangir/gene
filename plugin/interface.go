fcc640c6a93922b9e873979970a7ef0b14c6b3fc
Cihangir SAVAS <cihangir@koding.com>
2016-03-23 01:51:42 -0700

    plugin: add plugin generic functionality

---
+++ plugin/interface.go
@@ -0,0 +1,39 @@
+package plugin
+
+import (
+	"net/rpc"
+
+	"github.com/cihangir/gene/generators/common"
+	"github.com/cihangir/schema"
+)
+
+type Generator interface {
+	Generate(context *common.Context, s *schema.Schema) ([]common.Output, error)
+}
+
+type Request struct {
+	Context *common.Context
+	Schema  *schema.Schema
+}
+
+type GeneratorRPCServer struct {
+	// This is the real implementation
+	Impl Generator
+}
+
+func (g *GeneratorRPCServer) Generate(args Request, res []common.Output) (err error) {
+	res, err = g.Impl.Generate(args.Context, args.Schema)
+	return
+}
+
+// Here is an implementation that talks over RPC
+type GeneratorRPCClient struct{ Client *rpc.Client }
+
+func (g *GeneratorRPCClient) Generate(context *common.Context, s *schema.Schema) (res []common.Output, err error) {
+	args := &Request{
+		Context: context,
+		Schema:  s,
+	}
+
+	return res, g.Client.Call("Plugin.Generate", args, &res)
+}
