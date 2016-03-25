9e68b674d1f4a366f52b997bc97f3ddfdaa9d474
Cihangir SAVAS <cihangir@koding.com>
2016-03-24 23:22:23 -0700

    plugin: fix interface issues regarding net/rpc problem

--- plugin/interface.go
+++ plugin/interface.go
@@ -4,43 +4,38 @@ import (
 	"net/rpc"
 
 	"github.com/cihangir/gene/generators/common"
-	"github.com/cihangir/schema"
 	"github.com/hashicorp/go-plugin"
 )
 
-type Generator interface {
-	Generate(context *common.Context, s *schema.Schema) ([]common.Output, error)
-}
+type GeneratorRPCServer struct{ Impl common.Generator }
 
-type Request struct {
-	Context *common.Context
-	Schema  *schema.Schema
-}
-
-type GeneratorRPCServer struct {
-	// This is the real implementation
-	Impl Generator
-}
-
-func (g *GeneratorRPCServer) Generate(args Request, res []common.Output) (err error) {
-	res, err = g.Impl.Generate(args.Context, args.Schema)
-	return
+func (g *GeneratorRPCServer) Generate(req *common.Req, res *common.Res) error {
+	return g.Impl.Generate(req, res)
 }
 
 // Here is an implementation that talks over RPC
 type GeneratorRPCClient struct{ Client *rpc.Client }
 
-func (g *GeneratorRPCClient) Generate(context *common.Context, s *schema.Schema) (res []common.Output, err error) {
-	args := &Request{
-		Context: context,
-		Schema:  s,
-	}
-
-	return res, g.Client.Call("Plugin.Generate", args, &res)
+func (g *GeneratorRPCClient) Generate(req *common.Req, res *common.Res) error {
+	return g.Client.Call("Plugin.Generate", req, res)
 }
 
 var HandshakeConfig = plugin.HandshakeConfig{
 	ProtocolVersion:  1,
 	MagicCookieKey:   "BASIC_PLUGIN",
 	MagicCookieValue: "hello",
+}
+
+type GeneratorPlugin struct{ generator common.Generator }
+
+func NewGeneratorPlugin(g common.Generator) *GeneratorPlugin {
+	return &GeneratorPlugin{generator: g}
+}
+
+func (g *GeneratorPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
+	return &GeneratorRPCServer{Impl: g.generator}, nil
+}
+
+func (g *GeneratorPlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &GeneratorRPCClient{Client: c}, nil
 }
