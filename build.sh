f4d7f38e0fbc716a217a8e31449719df9cdd14d0
Cihangir SAVAS <cihangir@koding.com>
2016-03-25 23:13:43 -0700

    build: add plugin build steps

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ geneservices=(
   github.com/cihangir/gene/cmd/gene
   github.com/cihangir/gene/plugins/gene-models
   github.com/cihangir/gene/plugins/gene-rows
+  github.com/cihangir/gene/plugins/gene-errors
+  github.com/cihangir/gene/plugins/gene-kit
   github.com/cihangir/geneddl/cmd/gene-ddl
 )
 
