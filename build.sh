a06a122fe5c831f5a50da0dedf986fbd9564a6b9
Cihangir SAVAS <cihangir@koding.com>
2016-03-25 22:51:08 -0700

    build.sh: add new plugins

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -o errexit
 
 geneservices=(
   github.com/cihangir/gene/cmd/gene
+  github.com/cihangir/gene/plugins/gene-models
+  github.com/cihangir/gene/plugins/gene-rows
   github.com/cihangir/geneddl/cmd/gene-ddl
 )
 
