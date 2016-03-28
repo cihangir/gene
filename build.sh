09f4ada9f0128166e79570f9846e4f33911e7e72
Cihangir SAVAS <cihangir@koding.com>
2016-03-27 20:09:28 -0700

    build.sh: add gene-tests plugins into geneservices

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ geneservices=(
   github.com/cihangir/gene/plugins/gene-errors
   github.com/cihangir/gene/plugins/gene-kit
   github.com/cihangir/gene/plugins/gene-dockerfiles
+  github.com/cihangir/gene/plugins/gene-tests
+  github.com/cihangir/gene/plugins/gene-tests-funcs
   github.com/cihangir/geneddl/cmd/gene-ddl
 )
 
