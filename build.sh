5653a66cd981e85536946dacc74f0a15fea43f52
Cihangir SAVAS <cihangir@koding.com>
2016-03-31 23:02:53 -0700

    bulid.sh: compile js plugin

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ geneservices=(
   github.com/cihangir/gene/plugins/gene-dockerfiles
   github.com/cihangir/gene/plugins/gene-tests
   github.com/cihangir/gene/plugins/gene-tests-funcs
+  github.com/cihangir/gene/plugins/gene-js
 )
 
 `which go` install -v "${geneservices[@]}"
