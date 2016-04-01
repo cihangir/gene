3a03767d96773ea2a30936cc4c0edca763a7f701
Cihangir SAVAS <cihangir@koding.com>
2016-03-31 23:04:00 -0700

    gene/js: simplify js validator generation

--- generators/js/template.go
+++ generators/js/template.go
@@ -1,7 +1,7 @@
 package js
 
 // FunctionsTemplate provides the template for js clients of models
-var FunctionsTemplate = `{{$schema := .Schema}}{{$title := $schema.Title}}module.exports.{{.ModuleName}} = {
+var FunctionsTemplate = `{{$schema := .Schema}}{{$title := $schema.Title}}{{$moduleName := .ModuleName}}module.exports.{{$moduleName}} = {
   // New creates a new local {{ToUpperFirst $title}} js client
   {{ToUpperFirst $title}} = function(){}
 
@@ -17,28 +17,8 @@ var FunctionsTemplate = `{{$schema := .Schema}}{{$title := $schema.Title}}module
 
 {{range $funcKey, $funcValue := $schema.Functions}}
   {{ToUpperFirst $title}}.{{$funcKey}} = function(data, callback) {
-    // data should be type of {{Argumentize $funcValue.Properties.incoming}}
-{{if Equal "array" $funcValue.Properties.incoming.Type}}{{$incoming := index $funcValue.Properties.incoming.Items 0}}
-    for (var i = 0; i < data.length; i++){
-      if(err = {{$incoming.Title}}.validate(data[i])) {
-        return callback(err, null)
-      }
-    }
-{{else}}
-    if(err = {{ToUpperFirst $funcValue.Properties.incoming.Title}}.validate(data)) {
-      return callback(err, null)
-    }
-{{end}}
-
-    // send request to the server
-    // we got the response
-    var res = {}
-    // response should be type of {{Argumentize $funcValue.Properties.outgoing}}x{{if Equal "array" $funcValue.Properties.outgoing.Type}}{{$outgoing := index $funcValue.Properties.outgoing.Items 0}}
-    res = res.map(function(datum) {
-      return {{ToUpperFirst $outgoing.Title}}.map(datum);
-    });{{else}}
-    res = {{ToUpperFirst $funcValue.Properties.outgoing.Title}}.map(res){{end}}
-    callback(null, res)
+  {{GenerateJSValidator $funcValue.Properties.incoming}}
+    return processRequest('/{{ToLower $title}}/{{ToLower $funcKey}}', data, callback)
   }
 {{end}}
 }
